ba9b689f45c44a8bb7de0ca26762059a045057fc
shatyrbekova <perizat.shatyrbekova@gmail.com>
2021-08-26 16:49:11 +0200

    snack-5

--- 5.php
+++ 5.php
@@ -10,5 +10,16 @@
 <!-- Prendere un testo abbastanza lungo, contenente diverse frasi.
 Suddividere il testo in tanti paragrafi <p>: ad ogni punto corrisponde un
 nuovo paragrafo. -->
+<?php
+
+$paragrafi='<p>PHP (acronimo ricorsivo di "PHP: Hypertext Preprocessor", preprocessore di ipertesti; originariamente acronimo di "Personal Home Page") 
+è un linguaggio di scripting interpretato, originariamente concepito per la programmazione di pagine web dinamiche. </p>:
+<p>L'interprete PHP è un software libero distribuito sotto la PHP License.</p>:
+<p>Attualmente è principalmente utilizzato per sviluppare applicazioni web lato server, ma può essere usato anche per scrivere script 
+a riga di comando o applicazioni stand-alone con interfaccia grafica. Un esempio di software scritto in PHP è MediaWiki,
+ su cui si basano i progetti wiki della Wikimedia Foundation come Wikipedia.</p>:
+<p> Nato nel 1994 ad opera del danese Rasmus Lerdorf, PHP era in origine una raccolta di script CGI che permettevano una facile gestione delle pagine personali.</p>
+ <p> Secondo l'annuncio originale di PHP 1.0 da parte dell'autore sul newsgroup comp.infosystems.www.authoring.cgi il significato originario dell'acronimo era Personal Home Page.</p>'
+?>
 </body>
 </html>
